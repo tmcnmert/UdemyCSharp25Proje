1cce65901cd623a419470a693dc4ede010bab38c
tmcnmert <tmcnmert@gmail.com>
2025-03-28 16:51:21 +0300

    12.Proje Eklendi

---
+++ _12SqlSorguSistemi/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace _12SqlSorguSistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbdb = new System.Windows.Forms.ComboBox();
+            this.cmbtable = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 22);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(941, 218);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(761, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Çalıştır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(706, 212);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(761, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Sql Notları";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(695, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Veri Tabanı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(719, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tablolar:";
+            // 
+            // cmbdb
+            // 
+            this.cmbdb.FormattingEnabled = true;
+            this.cmbdb.Location = new System.Drawing.Point(797, 36);
+            this.cmbdb.Name = "cmbdb";
+            this.cmbdb.Size = new System.Drawing.Size(134, 26);
+            this.cmbdb.TabIndex = 6;
+            this.cmbdb.SelectedIndexChanged += new System.EventHandler(this.cmbdb_SelectedIndexChanged);
+            // 
+            // cmbtable
+            // 
+            this.cmbtable.FormattingEnabled = true;
+            this.cmbtable.Location = new System.Drawing.Point(798, 68);
+            this.cmbtable.Name = "cmbtable";
+            this.cmbtable.Size = new System.Drawing.Size(134, 26);
+            this.cmbtable.TabIndex = 7;
+            this.cmbtable.SelectedIndexChanged += new System.EventHandler(this.cmbtable_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(-2, 292);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(947, 243);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sonuç";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(41, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(459, 50);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "SQL VERİ TABANI ARAYÜZÜ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(1, 60);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(712, 237);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sorgu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(944, 535);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmbtable);
+            this.Controls.Add(this.cmbdb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbdb;
+        private System.Windows.Forms.ComboBox cmbtable;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ _12SqlSorguSistemi/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using iText.Kernel.Pdf;
+using iText.Layout.Element;
+using iText.Layout;
+using System.IO;
+using System.Diagnostics;
+
+namespace _12SqlSorguSistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=mert\\SQLEXPRESS;Initial Catalog=" + cmbdb.Text + ";Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+            baglanti.Open();
+
+            SqlCommand komut2 = new SqlCommand(richTextBox1.Text, baglanti);
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+
+            SqlDataAdapter da = new SqlDataAdapter("Select * from"+" "+cmbtable.Text,baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        void DB()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=mert\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select name from sys.databases", baglanti);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                cmbdb.Items.Add(dr[0].ToString());
+            }
+            baglanti.Close();
+        }
+        void TBL()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=mert\\SQLEXPRESS;Initial Catalog=" + cmbdb.Text + ";Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+
+            baglanti.Open();
+            SqlCommand komut1 = new SqlCommand("Select * from sys.Tables", baglanti);
+            SqlDataReader dr1 = komut1.ExecuteReader();
+            while (dr1.Read())
+            {
+                cmbtable.Items.Add(dr1[0].ToString());
+            }
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DB();
+            SqlConnection baglanti = new SqlConnection("Data Source=mert\\SQLEXPRESS;Initial Catalog=" + cmbdb.Text + ";Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+        }
+
+        private void cmbdb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbtable.Items.Clear();
+            TBL();
+        }
+
+        private void cmbtable_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Select * from " + " " + cmbtable.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // PDF'nin kaynak yolu (proje içindeki konumu)
+            string sourcePath = @"...";
+
+            // Hedef yol: Kullanıcının Masaüstü
+            string targetPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "SQL_Notlar__1736717868.pdf");
+
+            try
+            {
+                // PDF'yi hedef konuma kopyala
+                File.Copy(sourcePath, targetPath, true);
+
+                // PDF başarıyla kopyalandı mesajı
+                MessageBox.Show("PDF başarıyla indirildi ve masaüstüne kaydedildi!", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // PDF'yi otomatik olarak aç
+                Process.Start("explorer.exe", targetPath);
+            }
+            catch (Exception ex)
+            {
+                // Hata mesajı
+                MessageBox.Show("Hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
