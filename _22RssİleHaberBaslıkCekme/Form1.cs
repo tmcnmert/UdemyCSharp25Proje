4ed56d3f0f4c54000b53b24d41202800231057a6
tmcnmert <tmcnmert@gmail.com>
2025-03-30 10:56:46 +0300

    22.Proje Eklendi

---
+++ _22RssİleHaberBaslıkCekme/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace _22RssİleHaberBaslıkCekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void habergetir(string url)
+        {
+            listBox1.Items.Clear();
+            XmlTextReader xmloku = new XmlTextReader(url);
+            while (xmloku.Read())
+            {
+                if (xmloku.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku.ReadString());
+                }
+            }
+        }   
+        private void button3_Click(object sender, EventArgs e)
+        {
+            habergetir("http://www.hurriyet.com.tr/rss/anasayfa");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            habergetir("http://www.milatgazetesi.com/rss.php");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            habergetir("https://www.fotomac.com.tr/rss/anasayfa.xml");
+        }
+    }
+}
