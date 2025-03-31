0c0950be49c8bf2c3656b304df0ac465ab051335
tmcnmert <tmcnmert@gmail.com>
2025-03-31 11:16:26 +0300

    6.Proje Eklendi

---
+++ _06İliskiliVeriTabaniHaraketTabloVeSorgulamaUygulamasi/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _06İliskiliVeriTabaniHaraketTabloVeSorgulamaUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=MERT\\SQLEXPRESS;Initial Catalog=Udemy25Proje6;Integrated Security=True;TrustServerCertificate=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Execute Proje6", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
